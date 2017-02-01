0705cc03370dae2aa77c84f57e6a8e9c8b8bb4a5
Ross Brazuk <ross.brazuk@gmail.com>
2017-02-01 15:41:19 -0500

    first commit

---
+++ src/com/rbrazuk/springdemo/Coach.java
@@ -0,0 +1,8 @@
+package com.rbrazuk.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+}
---
+++ src/com/rbrazuk/springdemo/FortuneService.java
@@ -0,0 +1,6 @@
+package com.rbrazuk.springdemo;
+
+public interface FortuneService {
+	
+	public String getFortune();
+}
